97ad236d4cd5025b487c8c6e9ecb66cc5e759016
asadkomi <asadkomi@iastate.edu>
2021-09-11 13:52:58 -0500

    added integration test

---
+++ src/test/java/com/asadabdalla/soccerplayer/integration/IntegrationTest.java
@@ -0,0 +1,13 @@
+package com.asadabdalla.soccerplayer.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IntegrationTest {
+
+    @Test
+    void apiTest() {
+        //TODO
+    }
+}
